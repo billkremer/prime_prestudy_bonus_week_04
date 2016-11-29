f2753d0548de18c130ad8bfea53628f842b64ddd
Bill Kremer <bkrems12@gmail.com>
2016-11-28 21:22:33 -0600

    added answer to function call

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/*  Create a JavaScript file named 'script.js' and add a 'splinter' function. 
-The function should take in 2 numbers as arguments, multiply those numbers, 
-and save the result to a locally scoped variable called 'z'. Next, create an 
+/*  Create a JavaScript file named 'script.js' and add a 'splinter' function.
+The function should take in 2 numbers as arguments, multiply those numbers,
+and save the result to a locally scoped variable called 'z'. Next, create an
 array that stores the first argument, second argument, and z. Return that array.
 
 Try testing this by console logging the splinter function, passing in 1 and 2.
@@ -11,7 +11,7 @@ Check all of this in to a repo called "prime_prestudy_bonus_week_04"
 var splinter = function (a, b) {
 	var z = a * b;
 	return [a, b, z];
-
 }
 
 console.log(splinter(1,2));
+// > [1,2,2]
